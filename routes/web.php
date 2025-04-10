0e0045e0f5a715c96629a8fe4236d718e2ad8f8d
Auliarahma910 <yasifaputriprasetio@gmail.com>
2025-04-10 20:56:41 +0700

    Restore dari backup dan update file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BukuController;
 
+Route::get('/buku', [BukuController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
